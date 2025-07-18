332744a08954e77d05b7e58b3c3825dba91f7222
Sahana-DS <sahana2000ds@gmail.com>
2025-07-18 15:48:13 +0530

    sahana

--- Backend/server.js
+++ Backend/server.js
@@ -317,7 +317,7 @@ async function startServer() {
   try {
     await checkDatabaseConnection();
     await initializeDatabase();
-    app.listen(port, host, () => {
+    app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on http://${host}:${port}`);
 });
   } catch (error) {
